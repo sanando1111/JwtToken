6edf9c5231c08f48b50a4972ca06b17303300aa8
Wolverine <Wolverine@DESKTOP-942HOHM>
2020-01-24 00:11:27 +0530

    Updated the API and business logic
    Change-Id: Id2af94a29a05c67a842382488bc6bcf2091317af

--- demo/src/main/java/com/example/controller/HelloWorldController.java
+++ demo/src/main/java/com/example/controller/HelloWorldController.java
@@ -1,25 +1,34 @@
 package com.example.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.email.SendMailService;
+import com.example.exception.InternalServerErrorException;
 import com.example.jwt.JwtRequest;
 import com.example.jwt.JwtResponse;
 import com.example.jwt.JwtTokenUtil;
 import com.example.jwt.JwtUserDetailsService;
+import com.example.model.Cards;
 import com.example.model.Email;
 import com.example.model.OrderTransaction;
-
-import org.springframework.security.core.userdetails.UserDetails;
+import com.example.model.TaxProducts;
+import com.example.service.CardsService;
+import com.example.service.TaxProductsService;
 
 @RestController
 @CrossOrigin
@@ -35,6 +44,12 @@ public class HelloWorldController {
 	private JmsTemplate jmsTemplate;
 	@Autowired
 	private SendMailService sendMailService;
+
+	@Autowired
+	private TaxProductsService taxProductService;
+
+	@Autowired
+	private CardsService cardsService;
 
 	private static final Logger log = LoggerFactory.getLogger(HelloWorldController.class);
 
@@ -63,5 +78,48 @@ public class HelloWorldController {
 	public void sendMail(@RequestBody Email email) {
 		log.info("Sending an email");
 		sendMailService.sendMessage(email);
+	}
+
+	@RequestMapping(value = "/allcards", method = RequestMethod.GET)
+	public List<Cards> getAllCards() {
+
+		return cardsService.getAllCardDetails();
+	}
+
+	@RequestMapping(value = "/activecards", method = RequestMethod.GET)
+	public List<Cards> getAllActiveCards() {
+
+		return cardsService.getActiveCardDetails();
+	}
+
+	@ExceptionHandler(value = { InternalServerErrorException.class })
+	@RequestMapping(value = "/addproduct", method = RequestMethod.POST)
+	public String addTaxProduct(@RequestBody TaxProducts product) {
+
+		log.info("Add product request payload:" + product);
+		return taxProductService.addProducts(product);
+	}
+
+	@RequestMapping(value = "/updateproduct", method = RequestMethod.POST)
+	public String updateTaxProduct(@RequestBody TaxProducts product) {
+
+		return taxProductService.updateProductAmount(product);
+	}
+
+	@RequestMapping(value = "/deleteproduct", method = RequestMethod.POST)
+	public String deleteTaxProduct(@RequestBody TaxProducts product) {
+
+		return taxProductService.deleteProduct(product);
+	}
+
+	@RequestMapping(value = "/alltaxproducts", method = RequestMethod.GET)
+	public List<String> getAllTaxProducts() {
+		return taxProductService.getActiveTaxProducts();
+	}
+
+	@RequestMapping(value = "/getproductdetails", method = RequestMethod.GET)
+	public TaxProducts productDetails(@RequestParam String productName) {
+		log.info("Sending details for product:"+productName);
+		return taxProductService.getProductDetails(productName);
 	}
 }
